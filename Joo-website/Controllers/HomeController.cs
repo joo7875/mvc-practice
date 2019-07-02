92becae3ccd1530d0847bafadb9db0db2539a9ea
Hyewon Joo <joo7875@naver.com>
2019-07-02 13:39:30 -0700

    Update

--- Joo-website/Controllers/HomeController.cs
+++ Joo-website/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Joo_website.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,38 +37,30 @@ namespace Joo_website.Controllers
         {
             var catList = new List<Cat>
             {
-                            new Cat() { category = "cat", productId = 1, productName = "Cat Food 6kg", price = 22, weeklyBest = 6 },
-                            new Cat() { category = "cat", productId = 2, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
-                            new Cat() { category = "cat", productId = 3, productName = "Fish&Vege for Cat", price = 10, weeklyBest = 3 },
-                            new Cat() { category = "cat", productId = 4, productName = "Kitten Ball", price = 3, weeklyBest = 5 },
-                            new Cat() { category = "cat", productId = 5, productName = "Cat Nutritional Food 3kg", price = 15, weeklyBest = 4 },
-                            new Cat() { category = "cat", productId = 6, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
-                            new Cat() { category = "cat", productId = 7, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
+                            new Cat() { category = "cat", productId = 8, productName = "Cat Food 6kg", price = 22, weeklyBest = 6 },
+                            new Cat() { category = "cat", productId = 9, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
+                            new Cat() { category = "cat", productId = 10, productName = "Fish&Vege for Cat", price = 10, weeklyBest = 3 },
+                            new Cat() { category = "cat", productId = 11, productName = "Kitten Ball", price = 3, weeklyBest = 5 },
+                            new Cat() { category = "cat", productId = 12, productName = "Cat Nutritional Food 3kg", price = 15, weeklyBest = 4 },
+                            new Cat() { category = "cat", productId = 13, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
+                            new Cat() { category = "cat", productId = 14, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
             };
 
             return View(catList);
         }
 
-        public ActionResult Details()
+        public ActionResult Details(string id)
         {
-            var details = new List<PetShop>
-            {
-                new PetShop() { category = "dog", productId = 1, description = "Details"}
-                //new PetShop() { category = "dog", productId = 2, productImage = , description = ""},
-                //new PetShop() { category = "dog", productId = 3, productImage = , description = ""},
-                //new PetShop() { category = "dog", productId = 4, productImage = , description = ""},
-                //new PetShop() { category = "dog", productId = 5, productImage = , description = ""},
-                //new PetShop() { category = "dog", productId = 6, productImage = , description = ""},
-                //new PetShop() { category = "dog", productId = 7, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 1, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 2, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 3, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 4, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 5, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 6, productImage = , description = ""},
-                //new PetShop() { category = "cat", productId = 7, productImage = , description = ""}
-            };
-            return View(details);
+            var dir = Server.MapPath("/Content/Images");
+            var path = Path.Combine(dir, id + ".jpg");
+            //ViewBag.Image = base.File(path, "image/jpeg");
+
+            ViewBag.Image = "/Content/Images/" + id + ".jpg";
+            ViewBag.description = "Healthy Dogs and Cats";
+            return View();
+
+            //return base.File(path, "image/jpeg");
+            //return View(base.File(path, "image/jpeg"));
         }
     }
 }
