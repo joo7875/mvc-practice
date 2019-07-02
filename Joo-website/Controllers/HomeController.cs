bfe61fee78d6ad33d30817c083b5f5cfded0215f
Hyewon Joo <joo7875@naver.com>
2019-07-02 15:02:25 -0700

    Sum dog and cat datas

--- Joo-website/Controllers/HomeController.cs
+++ Joo-website/Controllers/HomeController.cs
@@ -18,36 +18,60 @@ namespace Joo_website.Controllers
             return View();
         }
 
-        public ActionResult DogPage()
+        public ActionResult PetPage(string petType)
         {
-            var dogList = new List<Dog>{
-                            new Dog() { category = "dog", productId = 1, productName = "Doggy Food 6kg", price = 22, weeklyBest = 6 },
-                            new Dog() { category = "dog", productId = 2, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
-                            new Dog() { category = "dog", productId = 3, productName = "Beef&Pork for Dog", price = 10, weeklyBest = 3 },
-                            new Dog() { category = "dog", productId = 4, productName = "Doggy Ball", price = 3, weeklyBest = 5 },
-                            new Dog() { category = "dog", productId = 5, productName = "Doggy Nutritional Food 3kg", price = 15, weeklyBest = 4 },
-                            new Dog() { category = "dog", productId = 6, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
-                            new Dog() { category = "dog", productId = 7, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
+            var petList = new List<PetShop>{
+                            new PetShop() { category = "dog", productId = 1, productName = "Doggy Food 6kg", price = 22, weeklyBest = 6 },
+                            new PetShop() { category = "dog", productId = 2, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
+                            new PetShop() { category = "dog", productId = 3, productName = "Beef&Pork for Dog", price = 10, weeklyBest = 3 },
+                            new PetShop() { category = "dog", productId = 4, productName = "Doggy Ball", price = 3, weeklyBest = 5 },
+                            new PetShop() { category = "dog", productId = 5, productName = "Doggy Nutritional Food 3kg", price = 15, weeklyBest = 4 },
+                            new PetShop() { category = "dog", productId = 6, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
+                            new PetShop() { category = "dog", productId = 7, productName = "Fruity Medicine", price = 20, weeklyBest = 7 },
+                            new PetShop() { category = "cat", productId = 1, productName = "Cat Food 6kg", price = 22, weeklyBest = 6 },
+                            new PetShop() { category = "cat", productId = 2, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
+                            new PetShop() { category = "cat", productId = 3, productName = "Fish&Vege for Cat", price = 10, weeklyBest = 3 },
+                            new PetShop() { category = "cat", productId = 4, productName = "Kitten Ball", price = 3, weeklyBest = 5 },
+                            new PetShop() { category = "cat", productId = 5, productName = "Cat Nutritional Food 3kg", price = 15, weeklyBest = 4 },
+                            new PetShop() { category = "cat", productId = 6, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
+                            new PetShop() { category = "cat", productId = 7, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
             };
 
-            return View(dogList);
-        }
+            if (string.IsNullOrEmpty(petType))
+                petType = "dog";
 
-        public ActionResult CatPage()
-        {
-            var catList = new List<Cat>
+            var retList = new List<PetShop>();
+
+            //int nIdx = 1;
+
+            foreach (PetShop item in petList)
             {
-                            new Cat() { category = "cat", productId = 8, productName = "Cat Food 6kg", price = 22, weeklyBest = 6 },
-                            new Cat() { category = "cat", productId = 9, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
-                            new Cat() { category = "cat", productId = 10, productName = "Fish&Vege for Cat", price = 10, weeklyBest = 3 },
-                            new Cat() { category = "cat", productId = 11, productName = "Kitten Ball", price = 3, weeklyBest = 5 },
-                            new Cat() { category = "cat", productId = 12, productName = "Cat Nutritional Food 3kg", price = 15, weeklyBest = 4 },
-                            new Cat() { category = "cat", productId = 13, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
-                            new Cat() { category = "cat", productId = 14, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
-            };
+                if (item.category == petType)
+                {
+                    //item.productId = nIdx;
+                    retList.Add(item);
+                    //nIdx++;
+                }
+            }
 
-            return View(catList);
+            return View(retList);
         }
+
+        //public ActionResult CatPage()
+        //{
+        //    var catList = new List<Cat>
+        //    {
+        //                    new Cat() { category = "cat", productId = 8, productName = "Cat Food 6kg", price = 22, weeklyBest = 6 },
+        //                    new Cat() { category = "cat", productId = 9, productName = "Chewing Gum", price = 5, weeklyBest = 2 },
+        //                    new Cat() { category = "cat", productId = 10, productName = "Fish&Vege for Cat", price = 10, weeklyBest = 3 },
+        //                    new Cat() { category = "cat", productId = 11, productName = "Kitten Ball", price = 3, weeklyBest = 5 },
+        //                    new Cat() { category = "cat", productId = 12, productName = "Cat Nutritional Food 3kg", price = 15, weeklyBest = 4 },
+        //                    new Cat() { category = "cat", productId = 13, productName = "Chewy Bone", price = 8, weeklyBest = 1 },
+        //                    new Cat() { category = "cat", productId = 14, productName = "Fruity Medicine", price = 20, weeklyBest = 7 }
+        //    };
+
+        //    return View(catList);
+        //}
 
         public ActionResult Details(string id)
         {
